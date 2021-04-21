993bda7cdf6e3088c8783a2a1a652bef67dbe819
public_host <public_host@a.com>
2021-04-21 22:06:35 +0800

    fix

--- impl.go
+++ impl.go
@@ -10,14 +10,10 @@ import (
 	"go/parser"
 	"go/printer"
 	"go/token"
-	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"text/template"
-
-	"golang.org/x/tools/imports"
 )
 
 // findInterface returns the import path and identifier of an interface.
@@ -34,91 +30,91 @@ func findInterface(iface string, srcDir string) (path string, id string, err err
 	return strings.Join(pathId[:len(pathId)-1], "."), pathId[len(pathId)-1], nil
 }
 
-func _findInterface(iface string, srcDir string) (path string, id string, err error) {
-	if len(strings.Fields(iface)) != 1 {
-		return "", "", fmt.Errorf("couldn't parse interface: %s", iface)
-	}
+// func _findInterface(iface string, srcDir string) (path string, id string, err error) {
+// 	if len(strings.Fields(iface)) != 1 {
+// 		return "", "", fmt.Errorf("couldn't parse interface: %s", iface)
+// 	}
 
-	srcPath := filepath.Join(srcDir, "__go_impl__.go")
+// 	srcPath := filepath.Join(srcDir, "__go_impl__.go")
 
-	if slash := strings.LastIndex(iface, "/"); slash > -1 {
-		// package path provided
-		dot := strings.LastIndex(iface, ".")
-		// make sure iface does not end with "/" (e.g. reject net/http/)
-		if slash+1 == len(iface) {
-			return "", "", fmt.Errorf("interface name cannot end with a '/' character: %s", iface)
-		}
-		// make sure iface does not end with "." (e.g. reject net/http.)
-		if dot+1 == len(iface) {
-			return "", "", fmt.Errorf("interface name cannot end with a '.' character: %s", iface)
-		}
-		// make sure iface has exactly one "." after "/" (e.g. reject net/http/httputil)
-		if strings.Count(iface[slash:], ".") != 1 {
-			return "", "", fmt.Errorf("invalid interface name: %s", iface)
-		}
-		return iface[:dot], iface[dot+1:], nil
-	}
+// 	if slash := strings.LastIndex(iface, "/"); slash > -1 {
+// 		// package path provided
+// 		dot := strings.LastIndex(iface, ".")
+// 		// make sure iface does not end with "/" (e.g. reject net/http/)
+// 		if slash+1 == len(iface) {
+// 			return "", "", fmt.Errorf("interface name cannot end with a '/' character: %s", iface)
+// 		}
+// 		// make sure iface does not end with "." (e.g. reject net/http.)
+// 		if dot+1 == len(iface) {
+// 			return "", "", fmt.Errorf("interface name cannot end with a '.' character: %s", iface)
+// 		}
+// 		// make sure iface has exactly one "." after "/" (e.g. reject net/http/httputil)
+// 		if strings.Count(iface[slash:], ".") != 1 {
+// 			return "", "", fmt.Errorf("invalid interface name: %s", iface)
+// 		}
+// 		return iface[:dot], iface[dot+1:], nil
+// 	}
 
-	src := []byte("package hack\n" + "var i " + iface)
-	// If we couldn't determine the import path, goimports will
-	// auto fix the import path.
+// 	src := []byte("package hack\n" + "var i " + iface)
+// 	// If we couldn't determine the import path, goimports will
+// 	// auto fix the import path.
 
-	log.Println(string(src))
-	imp, err := imports.Process(srcPath, src, nil)
-	if err != nil {
-		return "", "", fmt.Errorf("couldn't parse interface: %s", iface)
-	}
+// 	log.Println(string(src))
+// 	imp, err := imports.Process(srcPath, src, nil)
+// 	if err != nil {
+// 		return "", "", fmt.Errorf("couldn't parse interface: %s", iface)
+// 	}
 
-	// imp should now contain an appropriate import.
-	// Parse out the import and the identifier.
-	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, srcPath, imp, 0)
-	if err != nil {
-		panic(err)
-	}
+// 	// imp should now contain an appropriate import.
+// 	// Parse out the import and the identifier.
+// 	fset := token.NewFileSet()
+// 	f, err := parser.ParseFile(fset, srcPath, imp, 0)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	qualified := strings.Contains(iface, ".")
+// 	qualified := strings.Contains(iface, ".")
 
-	if len(f.Imports) == 0 && qualified {
-		return "", "", fmt.Errorf("unrecognized interface: %s", iface)
-	}
+// 	if len(f.Imports) == 0 && qualified {
+// 		return "", "", fmt.Errorf("unrecognized interface: %s", iface)
+// 	}
 
-	if !qualified {
-		// If !qualified, the code looks like:
-		//
-		// package hack
-		//
-		// var i Reader
-		decl := f.Decls[0].(*ast.GenDecl)      // var i io.Reader
-		spec := decl.Specs[0].(*ast.ValueSpec) // i io.Reader
-		sel := spec.Type.(*ast.Ident)
-		id = sel.Name // Reader
+// 	if !qualified {
+// 		// If !qualified, the code looks like:
+// 		//
+// 		// package hack
+// 		//
+// 		// var i Reader
+// 		decl := f.Decls[0].(*ast.GenDecl)      // var i io.Reader
+// 		spec := decl.Specs[0].(*ast.ValueSpec) // i io.Reader
+// 		sel := spec.Type.(*ast.Ident)
+// 		id = sel.Name // Reader
 
-		return path, id, nil
-	}
+// 		return path, id, nil
+// 	}
 
-	// If qualified, the code looks like:
-	//
-	// package hack
-	//
-	// import (
-	//   "io"
-	// )
-	//
-	// var i io.Reader
-	raw := f.Imports[0].Path.Value   // "io"
-	path, err = strconv.Unquote(raw) // io
-	if err != nil {
-		panic(err)
-	}
-	decl := f.Decls[1].(*ast.GenDecl)      // var i io.Reader
-	spec := decl.Specs[0].(*ast.ValueSpec) // i io.Reader
-	sel := spec.Type.(*ast.SelectorExpr)   // io.Reader
-	id = sel.Sel.Name                      // Reader
+// 	// If qualified, the code looks like:
+// 	//
+// 	// package hack
+// 	//
+// 	// import (
+// 	//   "io"
+// 	// )
+// 	//
+// 	// var i io.Reader
+// 	raw := f.Imports[0].Path.Value   // "io"
+// 	path, err = strconv.Unquote(raw) // io
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	decl := f.Decls[1].(*ast.GenDecl)      // var i io.Reader
+// 	spec := decl.Specs[0].(*ast.ValueSpec) // i io.Reader
+// 	sel := spec.Type.(*ast.SelectorExpr)   // io.Reader
+// 	id = sel.Sel.Name                      // Reader
 
-	log.Println(path, id)
-	return path, id, nil
-}
+// 	log.Println(path, id)
+// 	return path, id, nil
+// }
 
 // Pkg is a parsed build.Package.
 type Pkg struct {
